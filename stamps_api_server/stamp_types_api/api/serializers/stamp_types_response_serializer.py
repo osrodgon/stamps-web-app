56cb288279c5b923f13bb6d6f8003108836955c9
osrodgon <osrodgon@gmail.com>
2025-09-02 09:24:39 +0200

    Fixed typo in serializers

--- stamps_api_server/stamp_types_api/api/serializers/stamp_types_response_serializer.py
+++ stamps_api_server/stamp_types_api/api/serializers/stamp_types_response_serializer.py
@@ -5,4 +5,4 @@ from stamp_types_api.models import StampType
 class StampTypesResponseSerializer(serializers.Serializer):
     class Meta:
         model = StampType
-        fields = ['name']
+        fields = ['id','name']
