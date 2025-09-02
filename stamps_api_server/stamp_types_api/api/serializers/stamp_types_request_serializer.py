4522d47567aa295665e5ec57a01b9396d1033ae5
osrodgon <osrodgon@gmail.com>
2025-09-02 10:09:58 +0200

    Updated serializers for all APIs

--- stamps_api_server/stamp_types_api/api/serializers/stamp_types_request_serializer.py
+++ stamps_api_server/stamp_types_api/api/serializers/stamp_types_request_serializer.py
@@ -6,3 +6,12 @@ class StampTypesRequestSerializer(serializers.Serializer):
     class Meta:
         model = StampType
         fields = ['name']
+        
+    def to_internal_value(self, data):
+        # Check for unexpected fields
+        extra_fields = set(data.keys()) - set(self.fields.keys())
+        if extra_fields:
+            raise serializers.ValidationError(
+                {field: "This field is not allowed." for field in extra_fields}
+            )
+        return super().to_internal_value(data)
--- stamps_api_server/years_api/api/serializers/year_request_serializer.py
+++ stamps_api_server/years_api/api/serializers/year_request_serializer.py
@@ -5,3 +5,12 @@ class YearRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Year
         fields = ['year']
+        
+    def to_internal_value(self, data):
+        # Check for unexpected fields
+        extra_fields = set(data.keys()) - set(self.fields.keys())
+        if extra_fields:
+            raise serializers.ValidationError(
+                {field: "This field is not allowed." for field in extra_fields}
+            )
+        return super().to_internal_value(data)
