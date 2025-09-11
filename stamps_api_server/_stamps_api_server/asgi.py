0407182e1730f78f2c913ce6da5412e74e6917b0
osrodgon <osrodgon@gmail.com>
2025-09-11 17:07:43 +0200

    Project name changed to _stamps_api_server

--- stamps_api_server/_stamps_api_server/asgi.py
+++ stamps_api_server/_stamps_api_server/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for stamps_api_server project.
+ASGI config for _stamps_api_server project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stamps_api_server.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "_stamps_api_server.settings")
 
 application = get_asgi_application()
