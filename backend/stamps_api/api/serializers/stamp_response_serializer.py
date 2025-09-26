568fd26b5cdcae6a6720885001dc02f9cd04e147
osrodgon <osrodgon@gmail.com>
2025-09-26 16:14:59 +0200

    Updated StampResponseSerializer to use issue and colors string representation instead of IDs

--- backend/stamps_api/api/serializers/stamp_response_serializer.py
+++ backend/stamps_api/api/serializers/stamp_response_serializer.py
@@ -1,8 +1,12 @@
+from turtle import color
 from rest_framework import serializers
 from stamps_api.models import Stamp
 
 
 class StampResponseSerializer(serializers.ModelSerializer):
+    issue = serializers.StringRelatedField()
+    colors = serializers.StringRelatedField(many=True)
+    
     class Meta:
         model = Stamp
         fields = '__all__'
