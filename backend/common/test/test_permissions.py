ca2a83850a5a15b7699752728fd34f597075b394
osrodgon <osrodgon@gmail.com>
2025-10-13 12:44:24 +0200

    Updated import

--- backend/common/test/test_permissions.py
+++ backend/common/test/test_permissions.py
@@ -6,7 +6,7 @@ from rest_framework_api_key.models import APIKey
 # --- Mocking the External Dependencies ---
 
 # Assuming this is your custom permission class path
-from backend.common.core.permissions import HasSpecificKeyName
+from common.core.permissions import HasSpecificKeyName
 from common.api.messages import Messages 
 
 # Assuming you have a simplified version of your Messages class for testing
