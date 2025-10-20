87519aecd6f5d220cf1ea9d47ada032fbb2f22fb
osrodgon <osrodgon@gmail.com>
2025-10-20 23:19:46 +0200

    Finished updating issues unit tests

--- backend/issues_api/test/test_issues_api.py
+++ backend/issues_api/test/test_issues_api.py
@@ -1,4 +1,5 @@
 import os
+from sqlite3 import apilevel
 from unittest.mock import patch
 from urllib import response
 import pytest
@@ -155,17 +156,45 @@ class TestIssuesAPI(AbstractApiUnitTest):
         assert response.json()['errors'][0]['code'] == Messages.Code.connection_lost()
         assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
         
-    def test_get_issue_returns_data_200_ok(self, api_client, issues_table):
+    def test_get_one_issue_returns_200_ok_data(self, api_client, issues_table):
+        self.permission(granted=True)
         issue_id = issues_table[0].id
         response = api_client.get(f"{self.__get_url()}{issue_id}/")
 
-        assert response.json()['success'] is True
+        assert response.json()['success'] == True
         assert response.json()['message'] == Messages.retrieved_successfully()
-        assert response.json()['errors'] is None
+        assert response.json()['errors'] == None
         assert response.json()['data']['name'] == issues_table[0].name
         assert response.status_code == status.HTTP_200_OK
+        
+    def test_get_one_issue_returns_403_invalid_key(self, api_client, issues_table):
+        self.permission(granted=False, message=Messages.APIKey.invalid_key())
+        issue_id = issues_table[0].id
+        response = api_client.get(f"{self.__get_url()}{issue_id}/")
+
+        assert response.json()['success'] == False
+        assert response.json()['message'] == Messages.failed()
+        assert response.json()['data'] == None
+        assert response.json()['errors'][0]['field'] == 'detail'
+        assert response.json()['errors'][0]['message'] == Messages.APIKey.invalid_key()
+        assert response.json()['errors'][0]['code'] == Messages.Code.permission_denied()
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        
+    def test_get_one_issue_returns_403_invalid_user(self, api_client, issues_table):
+        self.permission(granted=False, message=Messages.APIKey.invalid_user())
+        issue_id = issues_table[0].id
+        response = api_client.get(f"{self.__get_url()}{issue_id}/")
+
+        assert response.json()['success'] == False
+        assert response.json()['message'] == Messages.failed()
+        assert response.json()['data'] == None
+        assert response.json()['errors'][0]['field'] == 'detail'
+        assert response.json()['errors'][0]['message'] == Messages.APIKey.invalid_user()
+        assert response.json()['errors'][0]['code'] == Messages.Code.permission_denied()
+        assert response.status_code == status.HTTP_403_FORBIDDEN
     
-    def test_get_issue_returns_no_data_404_not_found(self, api_client):
+    def test_get_issue_returns_404_not_found(self, api_client):
+        self.permission(granted=True)
         response = api_client.get(f"{self.__get_url()}999/")
 
         assert response.json()['success'] is False
@@ -174,65 +203,158 @@ class TestIssuesAPI(AbstractApiUnitTest):
         assert response.json()['errors'][0]['message'] == Messages.Get.not_found("issue", 999)
         assert response.status_code == status.HTTP_404_NOT_FOUND
         
-    def test_put_issue_updates_record_200_ok(self, api_client, issues_table, issue_put_payload_ok):
+    def test_get_issue_returns_500_database_error_connection_lost(self, api_client, issues_table):
+        self.permission(granted=True)
+        self.connection_lost(Issue, self.GET_ONE)
+        issue_id = issues_table[0].id
+        response = api_client.get(f"{self.__get_url()}{issue_id}/")
+
+        assert response.json()['success'] == False
+        assert response.json()['message'] == Messages.failed()
+        assert response.json()['data'] == None
+        assert response.json()['errors'][0]['field'] == 'detail'
+        assert response.json()['errors'][0]['message'] == Messages.Database.connection_lost()
+        assert response.json()['errors'][0]['code'] == Messages.Code.connection_lost()
+        assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        
+    def test_put_issue_returns_200_ok(self, api_client, issues_table, issue_put_payload_ok):
+        self.permission(granted=True)
         issue_id = issues_table[0].id
         response = api_client.put(f"{self.__get_url()}{issue_id}/", issue_put_payload_ok, format='json')
 
-        assert response.json()['success'] is True
+        assert response.json()['success'] == True
         assert response.json()['message'] == Messages.updated_successfully()
-        assert response.json()['errors'] is None
+        assert response.json()['errors'] == None
         assert response.json()['data']['name'] == issue_put_payload_ok["name"]
         assert response.status_code == status.HTTP_200_OK
         
-    def test_put_issue_updates_record_404_not_found(self, api_client, issue_put_payload_ok):
-        response = api_client.put(f"{self.__get_url()}999/", issue_put_payload_ok, format='json')
-
-        assert response.json()['success'] is False
-        assert response.json()['message'] == Messages.failed()
-        assert response.json()['data'] is None
-        assert response.json()['errors'][0]['message'] == Messages.Put.not_found("issue", 999)
-        assert response.status_code == status.HTTP_404_NOT_FOUND
-       
-    def test_put_issue_updates_record_400_bad_request(self, api_client, issues_table, issue_put_payload_ok):
+    def test_put_issue_returns_400_bad_request_invalid_field(self, api_client, issues_table, issue_put_payload_ok):
+        self.permission(granted=True)
         issue_id = issues_table[0].id
         issue_put_payload_ok["new_field"] = "new_value"
         response = api_client.put(f"{self.__get_url()}{issue_id}/", issue_put_payload_ok, format='json')
 
-        assert response.json()['success'] is False
+        assert response.json()['success'] == False
         assert response.json()['message'] == Messages.failed()
-        assert response.json()['data'] is None
+        assert response.json()['data'] == None
         assert response.json()['errors'][0]['field'] == 'new_field'
         assert response.json()['errors'][0]['message'] == Messages.field_not_allowed()
+        assert response.json()['errors'][0]['code'] == Messages.Code.invalid()
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         
-    def test_delete_issue_deletes_record_200_ok(self, api_client, issues_table):
+    def test_put_issue_returns_403_invalid_key(self, api_client, issues_table, issue_put_payload_ok):
+        self.permission(granted=False, message=Messages.APIKey.invalid_key())
+        issue_id = issues_table[0].id
+        response = api_client.put(f"{self.__get_url()}{issue_id}/", issue_put_payload_ok)
+        
+        assert response.json()['success'] == False
+        assert response.json()['message'] == Messages.failed()
+        assert response.json()['data'] == None
+        assert response.json()['errors'][0]['field'] == 'detail'
+        assert response.json()['errors'][0]['message'] == Messages.APIKey.invalid_key()
+        assert response.json()['errors'][0]['code'] == Messages.Code.permission_denied()
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        
+    def test_put_issue_returns_403_invalid_user(self, api_client, issues_table, issue_put_payload_ok):
+        self.permission(granted=False, message=Messages.APIKey.invalid_user())
+        issue_id = issues_table[0].id
+        response = api_client.put(f"{self.__get_url()}{issue_id}/", issue_put_payload_ok)
+        
+        assert response.json()['success'] == False
+        assert response.json()['message'] == Messages.failed()
+        assert response.json()['data'] == None
+        assert response.json()['errors'][0]['field'] == 'detail'
+        assert response.json()['errors'][0]['message'] == Messages.APIKey.invalid_user()
+        assert response.json()['errors'][0]['code'] == Messages.Code.permission_denied()
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        
+    def test_put_issue_returns_404_not_found(self, api_client, issue_put_payload_ok):
+        self.permission(granted=True)
+        response = api_client.put(f"{self.__get_url()}999/", issue_put_payload_ok, format='json')
+
+        assert response.json()['success'] == False
+        assert response.json()['message'] == Messages.failed()
+        assert response.json()['data'] == None
+        assert response.json()['errors'][0]['field'] == None
+        assert response.json()['errors'][0]['message'] == Messages.Put.not_found("issue", 999)
+        assert response.json()['errors'][0]['code'] == Messages.Code.other()
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        
+    def test_put_issue_returns_500_database_error_connection_lost(self, api_client, issues_table, issue_put_payload_ok):
+        self.permission(granted=True)
+        self.connection_lost(Issue, self.PUT)
+        issue_id = issues_table[0].id
+        response = api_client.put(f"{self.__get_url()}{issue_id}/", issue_put_payload_ok, format='json')
+        
+        assert response.json()['success'] == False
+        assert response.json()['message'] == Messages.failed()
+        assert response.json()['data'] == None
+        assert response.json()['errors'][0]['field'] == 'detail'
+        assert response.json()['errors'][0]['message'] == Messages.Database.connection_lost()
+        assert response.json()['errors'][0]['code'] == Messages.Code.connection_lost()
+        assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        
+    def test_delete_issue_returns_200_ok(self, api_client, issues_table):
+        self.permission(granted=True)
         issue_id = issues_table[0].id
         response = api_client.delete(f"{self.__get_url()}{issue_id}/")
 
-        assert response.json()['success'] is True
+        assert response.json()['success'] == True
         assert response.json()['message'] == Messages.deleted_successfully()
-        assert response.json()['errors'] is None
+        assert response.json()['errors'] == None
         assert response.json()['data']['message'] == Messages.Delete.deleted_one("issue", issue_id)
         assert response.status_code == status.HTTP_200_OK
         
-    def test_delete_issue_deletes_record_404_not_found(self, api_client):
+    def test_delete_issue_returns_403_invalid_key(self, api_client, issues_table):
+        self.permission(granted=False, message=Messages.APIKey.invalid_key())
+        issue_id = issues_table[0].id
+        response = api_client.delete(f"{self.__get_url()}{issue_id}/")
+        
+        assert response.json()['success'] == False
+        assert response.json()['message'] == Messages.failed()
+        assert response.json()['data'] == None
+        assert response.json()['errors'][0]['field'] == 'detail'
+        assert response.json()['errors'][0]['message'] == Messages.APIKey.invalid_key()
+        assert response.json()['errors'][0]['code'] == Messages.Code.permission_denied()
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        
+    def test_delete_issue_returns_403_invalid_user(self, api_client, issues_table):
+        self.permission(granted=False, message=Messages.APIKey.invalid_user())
+        issue_id = issues_table[0].id
+        response = api_client.delete(f"{self.__get_url()}{issue_id}/")
+        
+        assert response.json()['success'] == False
+        assert response.json()['message'] == Messages.failed()
+        assert response.json()['data'] == None
+        assert response.json()['errors'][0]['field'] == 'detail'
+        assert response.json()['errors'][0]['message'] == Messages.APIKey.invalid_user()
+        assert response.json()['errors'][0]['code'] == Messages.Code.permission_denied()
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        
+    def test_delete_issue_returns_404_not_found(self, api_client):
+        self.permission(granted=True)
         response = api_client.delete(f"{self.__get_url()}999/")
 
-        assert response.json()['success'] is False
+        assert response.json()['success'] == False
         assert response.json()['message'] == Messages.failed()
-        assert response.json()['data'] is None
+        assert response.json()['data'] == None
+        assert response.json()['errors'][0]['field'] == None
         assert response.json()['errors'][0]['message'] == Messages.Delete.not_found("issue", 999)
+        assert response.json()['errors'][0]['code'] == Messages.Code.other()
         assert response.status_code == status.HTTP_404_NOT_FOUND
         
-    @patch('issues_api.api.views.issues_by_id_view.Issue.objects.get')
-    def test_delete_issue_deletes_record_database_error(self, mock_get, api_client, issues_table):
+    def test_delete_issue_returns_500_database_error_connection_lost(self, api_client, issues_table):
+        self.permission(granted=True)
+        self.connection_lost(Issue, self.DELETE)
         issue_id = issues_table[0].id
-        mock_get.side_effect = Exception("Database connection lost")
         response = api_client.delete(f"{self.__get_url()}{issue_id}/")
 
-        assert response.json()['success'] is False
+        assert response.json()['success'] == False
         assert response.json()['message'] == Messages.failed()
-        assert response.json()['errors'][0]['message'] == Messages.server_error()
+        assert response.json()['data'] == None
+        assert response.json()['errors'][0]['field'] == 'detail'
+        assert response.json()['errors'][0]['message'] == Messages.Database.connection_lost()
+        assert response.json()['errors'][0]['code'] == Messages.Code.connection_lost()
         assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
         
     def test_issue_model_str_representation(self, issues_table):
