6768b3c9a1d6b428bdc75e181042a640e988f8e8
osrodgon <osrodgon@gmail.com>
2025-10-23 18:32:58 +0200

    Created condition types table, api and tests

--- backend/colors_api/api/views/colors_by_id_view.py
+++ backend/colors_api/api/views/colors_by_id_view.py
@@ -45,7 +45,7 @@ class ColorsByIdView(Logger, APIView):
                 ColorResponseSerializer,
                 name="RetrieveColorForbidden",
                 success=False,
-                description="Permission denied. You're likely missing X-API-Key or X-API-User headers."
+                description="Permission denied. You're likely missing X-API-Key header."
                 )   
         }
     )    
@@ -90,7 +90,7 @@ class ColorsByIdView(Logger, APIView):
                 ColorResponseSerializer,
                 name="ColorUpdateForbidden",
                 success=False,
-                description="Permission denied. You're likely missing X-API-Key or X-API-User headers."
+                description="Permission denied. You're likely missing X-API-Key header."
                 ),    
             status.HTTP_404_NOT_FOUND: standardized_response(
                 GenericResponseSerializer,
@@ -142,7 +142,7 @@ class ColorsByIdView(Logger, APIView):
                 ColorResponseSerializer,
                 name="ColorDeleteForbidden",
                 success=False,
-                description="Permission denied. You're likely missing X-API-Key or X-API-User headers."
+                description="Permission denied. You're likely missing X-API-Key header."
                 ),   
             status.HTTP_404_NOT_FOUND: standardized_response(
                 GenericResponseSerializer,
---
+++ backend/condition_types_api/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from condition_types_api.models import ConditionType
+
+# Register your models here.
+admin.register(ConditionType)
