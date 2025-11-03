de6a24b3bb272efadb3560ad2e38166ede18b111
osrodgon <osrodgon@gmail.com>
2025-11-03 23:02:01 +0100

    Modifications added

--- backend/collection_items_api/api/views/collection_items_view.py
+++ backend/collection_items_api/api/views/collection_items_view.py
@@ -7,7 +7,6 @@ from drf_spectacular.utils import extend_schema
 
 from common.api.messages import Messages
 from common.api.serializers.generic_response import GenericResponseSerializer, GenericResponse
-from common.core.api_key_utils import ApiKeyUtils
 from common.log.logger import Logger
 from common.core.schemas import standardized_response
 from collection_items_api.models import CollectionItem
