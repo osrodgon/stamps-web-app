1271231856ba0d544ca59376e659e7494224fc45
osrodgon <osrodgon@gmail.com>
2025-12-04 19:49:49 +0100

    First version on unit test for login page (100% working)

---
+++ frontend/tests/data/users_api_responses.py
@@ -0,0 +1,74 @@
+LOGIN_RESPONSE_200_SUCCESS = {
+    "data": {
+        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ",
+        "user": {
+            "id": 1,
+            "username": "testuser",
+            "email": "test@email.com",
+            "first_name": "Test",
+            "last_name": "User",
+            "registration_date": "2025-12-02T21:32:04.607Z",
+            "is_active": True
+        }
+    },
+    "success": True,
+    "errors": None,
+    "message": "Login successful"
+}
+
+LOGIN_RESPONSE_200_SUCCESS_MISSING_TOKEN = {
+    "data": {
+        "token": None,
+        "user": {
+            "id": 1,
+            "username": "testuser",
+            "email": "test@email.com",
+            "first_name": "Test",
+            "last_name": "User",
+            "registration_date": "2025-12-02T21:32:04.607Z",
+            "is_active": True
+        }
+    },
+    "success": True,
+    "errors": None,
+    "message": "Login successful"
+}
+
+LOGIN_RESPONSE_400_BAD_REQUEST = {
+    "success": False,
+    "message": "Request failed",
+    "errors": [
+        {
+        "field": "password",
+        "message": "This field is required.",
+        "code": "required"
+        }
+    ],
+    "data": None
+}
+
+LOGIN_RESPONSE_401_UNAUTHORIZED = {
+    "success": False,
+    "message": "Request failed",
+    "errors": [
+        {
+        "field": None,
+        "message": "Login failed username and/or password do not match.",
+        "code": "other"
+        }
+    ],
+    "data": None
+}
+
+LOGIN_RESPONSE_500_SERVER_ERROR = {
+    "success": False,
+    "message": "Request failed",
+    "errors": [
+        {
+        "field": 'detail',
+        "message": "Database connection lost.",
+        "code": "connection_lost"
+        }
+    ],
+    "data": None
+}
--- frontend/tests/test_login_page.py
+++ frontend/tests/test_login_page.py
@@ -1,130 +1,230 @@
-from io import BytesIO
-import json
-from unittest.mock import MagicMock
-import nicegui
+import logging
 import pytest
 from nicegui.testing import User
-from starlette.middleware.sessions import SessionMiddleware
-from nicegui import app
-import requests
 
 from components.auth.login_card import LoginCard
 from pages.auth.login_page import LoginPage
-
-class AbstractUnitTest:
-    __mocker = None
-    __mocker_instance = None
-        
-    def __get_class_path(self, cls: object) -> str:
-        """Constructs the full import path for a given class object.
-
-        This private helper method is used to dynamically create the string
-        path needed for mocking objects with `mocker.patch`.
-
-        Args:
-            cls (object): The class object for which to get the import path.
-
-        Returns:
-            str: The full import path of the class (e.g., 'module.submodule.ClassName').
-        """
-        return f"{cls.__module__}.{cls.__name__}"
-    
-    def get_component(self, page, object):
-        for key, component in page.elements.items():
-            if isinstance(component, object):
-                return component
-        return None
-    
-    def set_card_valid(self, card, valid):
-        return self.__mocker.patch(f"{self.__get_class_path(card)}.is_valid", return_value=valid)
-    
-    def set_backend_response(self, class_object, status_code, json_data):
-        if json_data is not None:
-            json_bytes = json.dumps(json_data).encode('utf-8')
-        
-            response = requests.Response()
-            response.status_code = status_code
-            response._content = json_bytes
-            response.raw = BytesIO(json_bytes)
-            response.encoding = 'utf-8'
-            response.headers['Content-Type'] = 'application/json'
-        else:
-            response = None
-        
-        return self.__mocker.patch(f"{self.__get_class_path(class_object)}._make_request", return_value=response)
-    
-    def skip_notify(self, class_object):
-        return self.__mocker.patch(f"{self.__get_class_path(class_object)}.notify", return_value=None)   
-    
-    def create_user_storage(self, __mocker):
-        mock_user_storage = {}
-        mock_storage = MagicMock()
-        mock_storage.user = mock_user_storage
-
-        __mocker.patch.object(
-            nicegui.app, # The class/object whose attribute you want to replace
-            'storage',     # The attribute name
-            new=mock_storage # The mock object to use instead
-            )
-        return mock_user_storage
-    
-
-
-
-
-login_api_response = {
-    "data": {
-        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ",
-        "user": {
-            "id": 1,
-            "username": "testuser",
-            "email": "test@email.com",
-            "first_name": "Test",
-            "last_name": "User",
-            "registration_date": "2025-12-02T21:32:04.607Z",
-            "is_active": True
-        }
-    },
-    "success": True,
-    "errors": None,
-    "message": "Login successful"
-}
+from tests.helpers.abstract_unit_test import AbstractUnitTest
+from frontend.tests.data.users_api_responses import (
+    LOGIN_RESPONSE_200_SUCCESS, 
+    LOGIN_RESPONSE_200_SUCCESS_MISSING_TOKEN,
+    LOGIN_RESPONSE_400_BAD_REQUEST,
+    LOGIN_RESPONSE_401_UNAUTHORIZED,
+    LOGIN_RESPONSE_500_SERVER_ERROR
+)
 
 @pytest.mark.asyncio
 class TestLoginPage(AbstractUnitTest):
     @pytest.fixture(autouse=True)
-    def init_data(self, mocker):
+    def init_data(self, mocker, caplog):
+        caplog.set_level(logging.CRITICAL)
         self._AbstractUnitTest__mocker = mocker
+        
     
     async def test_login_success_200(self, user: User, mocker):
-        # self._AbstractUnitTest__mocker = mocker
+        # Open login page
         login_object = await user.open('/login')
         
-
+        # Get components
         login_card = self.get_component(login_object, LoginCard)
         login_page = self.get_component(login_object, LoginPage)
         
-        self.set_card_valid(card=login_card.__class__, valid=True)
-        self.set_backend_response(class_object=login_page.__class__, json_data=login_api_response, status_code=200)
+        # Set username and password
+        login_card.username.value = 'testuser'
+        login_card.password.value = 'testpassword'
+        
+        # Set mocks
+        # self.set_card_valid(card=login_card.__class__, valid=True)
+        self.set_backend_response(
+            class_object=login_page.__class__, 
+            json_data=LOGIN_RESPONSE_200_SUCCESS, 
+            status_code=200
+        )
         self.skip_notify(class_object=login_card.__class__)
         user_storage = self.create_user_storage(mocker)
         
+        # Assert components
         assert login_card is not None
         assert login_page is not None
         
-        # Simulates user hitting Login button
+        # Simulates user hitting the submit button
         response = await login_page.call_rest_method()
         
+        # Assert response
         assert response.status_code == 200
-        assert response.json() == login_api_response
-        assert user_storage['jwt_token'] == login_api_response['data']['token']
+        assert response.json() == LOGIN_RESPONSE_200_SUCCESS
+        assert user_storage['jwt_token'] == LOGIN_RESPONSE_200_SUCCESS['data']['token']
         
-    async def test_login_fail_400(self, user: User, mocker):
-        pass
+    async def test_login_success_200_missing_token(self, user: User):
+        # Open login page
+        login_object = await user.open('/login')
+        
+        # Get components
+        login_card = self.get_component(login_object, LoginCard)
+        login_page = self.get_component(login_object, LoginPage)
+        
+        # Set username and password
+        login_card.username.value = 'testuser'
+        login_card.password.value = 'testpassword'
+        
+        # Set mocks
+        self.set_backend_response(
+            class_object=login_page.__class__, 
+            json_data=LOGIN_RESPONSE_200_SUCCESS_MISSING_TOKEN, 
+            status_code=200
+        )
+        self.skip_notify(class_object=login_card.__class__)
+        
+        # Assert components
+        assert login_card is not None
+        assert login_page is not None
+        
+        # Simulates user hitting the submit button
+        response = await login_page.call_rest_method()
+        
+        # Assert response
+        assert response == "Login failed."
+        
+    async def test_login_fail_no_response(self, user: User):
+        # Open login page
+        login_object = await user.open('/login')
+        
+        # Get components
+        login_card = self.get_component(login_object, LoginCard)
+        login_page = self.get_component(login_object, LoginPage)
+        
+        # Set username and password
+        login_card.username.value = 'testuser'
+        login_card.password.value = 'testpassword'
+        
+        # Set mocks
+        self.set_backend_response(
+            class_object=login_page.__class__, 
+            json_data=None, 
+            status_code=500
+        )
+        self.skip_notify(class_object=login_card.__class__)
+        self.skip_notify(class_object=login_page.__class__)
+        
+        # Assert components
+        assert login_card is not None
+        assert login_page is not None
+        
+        # Simulates user hitting the submit button
+        response = await login_page.call_rest_method()
+        
+        # Assert response
+        assert response == "Login request failed, no response from server."
     
-    async def test_login_fail_401(self, user: User, mocker):
-        pass
+    async def test_login_fail_bad_request_400(self, user: User):
+        # Open login page
+        login_object = await user.open('/login')
+        
+        # Get components
+        login_card = self.get_component(login_object, LoginCard)
+        login_page = self.get_component(login_object, LoginPage)
+        
+        # Set username and password
+        login_card.username.value = 'testuser'
+        login_card.password.value = 'testpassword'
+        
+        # Set mocks
+        self.set_backend_response(
+            class_object=login_page.__class__, 
+            json_data=LOGIN_RESPONSE_400_BAD_REQUEST, 
+            status_code=400
+        )
+        self.skip_notify(class_object=login_card.__class__)
+        
+        # Assert components
+        assert login_card is not None
+        assert login_page is not None
+        
+        # Simulates user hitting the submit button
+        response = await login_page.call_rest_method()
+                
+        # Assert response
+        assert response.status_code == 400
+        assert response.json() == LOGIN_RESPONSE_400_BAD_REQUEST
     
-    async def test_login_fail_501(self, user: User, mocker):
-        pass
+    async def test_login_fail_unauthorized_401(self, user: User):
+        # Open login page
+        login_object = await user.open('/login')
+        
+        # Get components
+        login_card = self.get_component(login_object, LoginCard)
+        login_page = self.get_component(login_object, LoginPage)
+        
+        # Set username and password
+        login_card.username.value = 'testuser'
+        login_card.password.value = 'testpassword'
+        
+        # Set mocks
+        self.set_backend_response(
+            class_object=login_page.__class__, 
+            json_data=LOGIN_RESPONSE_401_UNAUTHORIZED, 
+            status_code=401
+        )
+        self.skip_notify(class_object=login_card.__class__)
+        
+        # Assert components
+        assert login_card is not None
+        assert login_page is not None
+        
+        # Simulates user hitting the submit button
+        response = await login_page.call_rest_method()
+                
+        # Assert response
+        assert response.status_code == 401
+        assert response.json() == LOGIN_RESPONSE_401_UNAUTHORIZED
     
+    async def test_login_fail_server_error_500(self, user: User):
+        # Open login page
+        login_object = await user.open('/login')
+        
+        # Get components
+        login_card = self.get_component(login_object, LoginCard)
+        login_page = self.get_component(login_object, LoginPage)
+        
+        # Set username and password
+        login_card.username.value = 'testuser'
+        login_card.password.value = 'testpassword'
+        
+        # Set mocks
+        self.set_backend_response(
+            class_object=login_page.__class__, 
+            json_data=LOGIN_RESPONSE_500_SERVER_ERROR, 
+            status_code=500
+        )
+        self.skip_notify(class_object=login_card.__class__)
+        
+        # Assert components
+        assert login_card is not None
+        assert login_page is not None
+        
+        # Simulates user hitting the submit button
+        response = await login_page.call_rest_method()
+                
+        # Assert response
+        assert response.status_code == 500
+        assert response.json() == LOGIN_RESPONSE_500_SERVER_ERROR
+    
+    async def test_login_fail_validation_fails(self, user: User):
+        # Open login page
+        login_object = await user.open('/login')
+        
+        # Get components
+        login_card = self.get_component(login_object, LoginCard)
+        login_page = self.get_component(login_object, LoginPage)
+        
+        # Set mocks
+        self.skip_notify(class_object=login_card.__class__)
+        
+        # Assert components
+        assert login_card is not None
+        assert login_page is not None
+        
+        # Simulates user hitting the submit button
+        response = await login_page.call_rest_method()
+        
+        assert response == "Username and password are required."
