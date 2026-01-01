e4cef6b8298089ce0e902f04424498792ea8df3b
osrodgon <osrodgon@gmail.com>
2026-01-01 12:14:21 +0100

    Added filters to issues API
    - Filter by year
    - Filter by issue name
    - Extra: Added new CSV data file

--- backend/issues_api/api/views/issues_view.py
+++ backend/issues_api/api/views/issues_view.py
@@ -3,12 +3,14 @@ from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from drf_spectacular.types import OpenApiTypes
 
 from common.api.serializers.generic_response import GenericResponseSerializer, GenericResponse
 from common.log.logger import Logger
 from common.core.schemas import standardized_response
 from common.api.messages import Messages
+from years_api.models import Year
 from issues_api.models import Issue
 from issues_api.api.serializers.issue_request_serializer import IssueRequestSerializer
 from issues_api.api.serializers.issue_response_serializer import IssueResponseSerializer
@@ -26,7 +28,23 @@ class IssuesView(Logger, APIView):
     @extend_schema(
         operation_id="list_issues",
         summary="List all issues",
-        description="Retrieves a list of all issue entries currently stored in the database.",
+        description="Retrieves a list of all issue entries. Can be filtered by year and name.",
+        parameters=[
+        OpenApiParameter(
+                name='year', 
+                type=OpenApiTypes.INT, 
+                location=OpenApiParameter.QUERY, 
+                description='Filter issues by year',
+                required=False
+            ),
+            OpenApiParameter(
+                name='name', 
+                type=OpenApiTypes.STR, 
+                location=OpenApiParameter.QUERY, 
+                description='Filter issues by name',
+                required=False
+            ),
+        ],
         responses={
             status.HTTP_200_OK: standardized_response(
                 IssueResponseSerializer, 
@@ -56,7 +74,18 @@ class IssuesView(Logger, APIView):
             with a 200 OK status.
         """
         self.log.debug(Messages.Get.retrieve_all("issues"))
-        issues = Issue.objects.all()
+        year = request.query_params.get('year', None)
+        issue_name = request.query_params.get('name', None)
+        
+        issues = Issue.objects.all().order_by('date')
+        if year:
+            self.log.debug(f"Filtering issues by year {year}")
+            year_id = Year.objects.all().filter(year=year).first().id
+            issues = issues.filter(year=year_id)
+        if issue_name:
+            self.log.debug(f"Filtering issues by issue name {issue_name}")
+            issues = issues.filter(name__icontains=issue_name)
+        
         self.log.debug(Messages.Get.retrieved_all("issues", issues.count()))
         response = IssueResponseSerializer(issues, many=True)
         
