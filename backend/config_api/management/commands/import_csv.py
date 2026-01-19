7d11325c0fa7298f11710810bfd003ed2dd67be4
osrodgon <osrodgon@gmail.com>
2026-01-19 20:07:34 +0100

    Import csv
    - Made changes to use the new csv format
    - AFter import is done, a json file is created so the data can be exported to other locations

--- backend/config_api/management/commands/import_csv.py
+++ backend/config_api/management/commands/import_csv.py
@@ -1,17 +1,20 @@
-from datetime import datetime
 import os
 import re
+from datetime import datetime
+
+import pandas as pd
+from colors_api.models import Color
+from countries_api.models import Country
+from django.conf import settings
+from django.core.management import call_command
 from django.core.management.base import BaseCommand
 from django.db import transaction
-import pandas as pd
-
-from years_api.models import Year
-from print_types_api.models import PrintType
-from colors_api.models import Color
 from issues_api.models import Issue
-from stamps_api.models import Stamp
-from countries_api.models import Country
+from print_types_api.models import PrintType
 from stamp_types_api.models import StampType
+from stamps_api.models import Stamp
+from years_api.models import Year
+
 
 class Command(BaseCommand):
     help = 'Imports data from csv file.'
@@ -55,7 +58,7 @@ class Command(BaseCommand):
         if len(date_str) == 4 and date_str.isdigit():
             return f"{date_str}-01-01"
         
-        for formato in ["%d/%m/%Y", "%d-%m-%Y", "%Y-%m-%d"]:
+        for formato in ["%d/%m/%Y", "%d-%m-%Y", "%Y-%m-%d", "%Y/%m/%d"]:
             try:
                 dt = datetime.strptime(date_str, formato)
                 return dt.strftime("%Y-%m-%d")
@@ -93,36 +96,61 @@ class Command(BaseCommand):
                 return match.group(1).strip()
                     
         return None
+    
+    def export_to_json(self, *args, **options):
+        # Define the output file path
+        output_file = os.path.join(settings.BASE_DIR, 'resources/full_project_export.json')
+        
+        # We exclude contenttypes and permissions because they are 
+        # recreated automatically by Django and often cause "Duplicate ID" 
+        # errors when you try to load the JSON later.
+        exclude_apps = ['contenttypes', 'auth.permission']
+
+        self.stdout.write(f"Starting export of all apps to {output_file}...")
+
+        with open(output_file, 'w', encoding='utf-8') as f:
+            call_command(
+                'dumpdata', 
+                format='json', 
+                indent=4, 
+                exclude=exclude_apps,
+                stdout=f
+            )
+
+        self.stdout.write(self.style.SUCCESS(f'Successfully exported all data!'))
 
     def handle(self, *args, **options):
-        df = pd.read_csv("dataset/sellos_espana.csv")
+        df = pd.read_csv("backend/resources/stamps.csv", sep='|')
         
         country_obj, _ = Country.objects.get_or_create(name="España")
         
         with transaction.atomic():
             for index, row in df.iterrows():
                 try:
-                    total_printed = self.clean_number(row['tirada'])
-                    date = self.clean_date(row['fecha_emision'])
-                    stamp_name = self.clean_string(row['nombre_sello'])
-                    issue_raw = self.clean_string(row['serie'])
-                    issue_name = issue_raw if issue_raw else stamp_name
-                    perforation = self.clean_string(row['perforacion'])
-                    description = self.clean_string(row['descripcion'])
-                    face_value = self.clean_string(row['valor_facial'])
-                    print_type = self.clean_string(row['impresion'])
-                    colors_raw = self.clean_string(row['colores'])
-                    stamp_type = self.clean_string(row['formato'])
-                    edifil_code = self.get_edifil_code(row['codigos'])
+                    issue_year = row['issue_year']
                     
-                    year_val = row['año']
-                    year_obj = None
-                    if pd.notna(year_val):
-                        year_obj, _ = Year.objects.get_or_create(year=int(year_val))
+                    edifil_code = self.clean_string(row['edifil_code'])
+                    issue_date = self.clean_date(row['issue_date'])
+                    temp_issue_name = self.clean_string(row['issue_name'])
+                    if temp_issue_name:
+                        issue_name = temp_issue_name
+                    issue_amount_printed = self.clean_number(row['issue_amount_printed'])
+                    issue_description = self.clean_string(row['issue_description'])
+                    
+                    stamp_print_type = self.clean_string(row['stamp_print_type'])
+                    stamp_type = self.clean_string(row['stamp_type'])
+                    stamp_perforation = self.clean_string(row['stamp_perforation'])
+                    stamp_colors_raw = self.clean_string(row['stamp_color'])
+                    stamp_name = self.clean_string(row['stamp_name'])
+                    stamp_face_value = self.clean_string(row['stamp_face_value'])
+                    
+                    issue_year_obj = None
+                    if pd.notna(issue_year):
+                        issue_year_obj, _ = Year.objects.get_or_create(year=int(issue_year))
                         
-                    print_obj = None
-                    if pd.notna(row['impresion']):
-                        print_obj, _ = PrintType.objects.get_or_create(name=print_type)
+                    issue_print_type_obj = None
+                    if pd.notna(row['stamp_print_type']):
+                        issue_print_type_obj, _ = PrintType.objects.get_or_create(name=stamp_print_type)
                         
                     stamp_type_obj = None
                     if pd.notna(stamp_type):
@@ -131,31 +159,32 @@ class Command(BaseCommand):
                         
                     issue_obj, created = Issue.objects.get_or_create(
                         name=issue_name,
-                        date=date,
-                        total_printed=total_printed,
+                        date=issue_date,
+                        total_printed=issue_amount_printed,
                         defaults={
-                            'year': year_obj,
-                            'perforation': perforation,
-                            'description': description,
+                            'year': issue_year_obj,
+                            'perforation': stamp_perforation,
+                            'print_type': issue_print_type_obj,
+                            'description': issue_description,
                             'country': country_obj,
                             'stamp_type': stamp_type_obj
                         }
                     )
 
                     if edifil_code:
-                        image = f"{year_val}/{edifil_code}.jpg"
+                        image = f"{issue_year}/{edifil_code}.jpg"
                     else:
-                        image = f"{year_val}/{stamp_name.replace(' ','-')}.jpg"
+                        image = f"{issue_year}/{stamp_name.replace(' ','-')}.jpg"
                     stamp_obj, _ = Stamp.objects.get_or_create(
                         issue = issue_obj,
                         name = stamp_name,
-                        face_value = face_value,
+                        face_value = stamp_face_value,
                         image = image,
                         edifil_code = edifil_code
                     )
                         
-                    if colors_raw:
-                        colors = self.clean_and_get_colors(colors_raw)
+                    if stamp_colors_raw:
+                        colors = self.clean_and_get_colors(stamp_colors_raw)
                         
                         color_objects = []
                         for color in colors:
@@ -170,4 +199,7 @@ class Command(BaseCommand):
                 except Exception as e:
                     print(f"❌ Error in row {index} ({row.get('nombre_sello')}): {e}")
             
-        print(f"Migration completed. {index} rows processed.")
+        print(f"Migration completed. {index} rows processed.\n")
+        
+        print("Exporting data to json...")
+        self.export_to_json()
